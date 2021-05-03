3175b023d47730caa2ff31bec44acb56217d1a37
Darkuantum <nathan,ly,sg@gmail.com>
2021-05-03 18:20:31 +0800

    Added some js to host the the website dynamically instead of statically, but for now it is not working as intended

--- main.js
+++ main.js
@@ -1,5 +1,21 @@
-function test() {
-    console.log("Hello world!");
-}
+const http = require("http");
+const url = require("url");
+const fs = require("fs"); 
 
-test();
+http.createServer((req, res) => {
+    var q = url.parse(req.url, true);
+    var page = (q.pathname == "/") ? "index.html" : `.${q.pathname}`;
+
+    console.log(q);
+    console.log(page);
+
+    fs.readFile(page, (err, data) => {
+        if (err) {
+            res.writeHead(404, {"Content-Type": "text/html"});
+            return res.end("404 Not Found");
+        }
+        res.writeHead(200, {"Content-Type": "text/html"});
+        res.write(data);
+        return res.end();
+    });
+}).listen(8080);
